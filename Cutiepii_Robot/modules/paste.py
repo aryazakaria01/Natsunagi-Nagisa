db7a68f560cf59cdf31a6bfe0c6d6368cb5e29aa
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-11-09 16:41:27 +0700

    Update paste.py

--- Cutiepii_Robot/modules/paste.py
+++ Cutiepii_Robot/modules/paste.py
@@ -1,134 +1,41 @@
-import codecs
-import asyncio
-import os
-import re
-
-import aiofiles
-from pykeyboard import InlineKeyboard
-from pyrogram import filters
-from pyrogram.types import InlineKeyboardButton
-
-from Cutiepii_Robot import pgram, aiohttpsession, dispatcher 
-from Cutiepii_Robot.utils.errors import capture_err
-from Cutiepii_Robot.utils.pastebin import paste
-from Cutiepii_Robot.modules.helper_funcs.alternate import typing_action, send_action
+import requests
+from Cutiepii_Robot import dispatcher
 from Cutiepii_Robot.modules.disable import DisableAbleCommandHandler
+from telegram import ParseMode, Update
+from telegram.ext import CallbackContext, run_async
 
-pattern = re.compile(
-    r"^text/|json$|yaml$|xml$|toml$|x-sh$|x-shellscript$"
-)
-
-
-async def isPreviewUp(preview: str) -> bool:
-    for _ in range(7):
-        try:
-            async with aiohttpsession.head(preview, timeout=2) as resp:
-                status = resp.status
-                size = resp.content_length
-        except asyncio.exceptions.TimeoutError:
-            return False
-        if status == 404 or (status == 200 and size == 0):
-            await asyncio.sleep(0.4)
-        else:
-            return status == 200
-    return False
-
-
-@pgram.on_message(filters.command("paste") & ~filters.edited)
-@capture_err
-async def paste_func(_, message):
-    if not message.reply_to_message:
-        return await message.reply_text(
-            "Reply To A Message With /paste"
-        )
-    m = await message.reply_text("Pasting...")
-    if message.reply_to_message.text:
-        content = str(message.reply_to_message.text)
-    elif message.reply_to_message.document:
-        document = message.reply_to_message.document
-        if document.file_size > 1048576:
-            return await m.edit(
-                "You can only paste files smaller than 1MB."
-            )
-        if not pattern.search(document.mime_type):
-            return await m.edit("Only text files can be pasted.")
-        doc = await message.reply_to_message.download()
-        async with aiofiles.open(doc, mode="r") as f:
-            content = await f.read()
-        os.remove(doc)
-    link = await paste(content)
-    preview = link + "/preview.png"
-    button = InlineKeyboard(row_width=1)
-    button.add(InlineKeyboardButton(text="Paste Link", url=link))
-
-    if await isPreviewUp(preview):
-        try:
-            await message.reply_photo(
-                photo=preview, quote=False, reply_markup=button
-            )
-            return await m.delete()
-        except Exception:
-            pass
-    return await m.edit(link)
-
-@typing_action
-def paste(update, context):
+def paste(update: Update, context: CallbackContext):
+    args = context.args
     message = update.effective_message
-    bot, args = context.bot, context.args
 
-    if not message.reply_to_message.text:
-        file = bot.getFile(message.reply_to_message.document)
-        file.download("file.txt")
-        text = codecs.open("file.txt", "r+", encoding="utf-8")
-        paste_text = text.read()
-        print(paste_text)
-        os.remove("file.txt")
+    if message.reply_to_message:
+        data = message.reply_to_message.text
 
-    elif message.reply_to_message.text:
-        paste_text = message.reply_to_message.text
     elif len(args) >= 1:
-        paste_text = message.text.split(None, 1)[1]
+        data = message.text.split(None, 1)[1]
 
     else:
-        message.reply_text(
-            "reply to any message or just do /paste <what you want to paste>"
-        )
+        message.reply_text("What am I supposed to do with this?")
         return
 
-    extension = "txt"
-    url = "https://spaceb.in/api/v1/documents/"
-    try:
-        response = requests.post(
-            url, data={"content": paste_text, "extension": extension}
-        )
-    except Exception as e:
-        return {"error": str(e)}
-
-    response = response.json()
-    text = (
-        f"**Pasted to [Space.bin]('https://spaceb.in/{response['payload']['id']}')!!!**"
+    key = (
+        requests.post("https://nekobin.com/api/documents", json={"content": data})
+        .json()
+        .get("result")
+        .get("key")
     )
-    buttons = [
-        [
-            InlineKeyboardButton(
-                text="View Link", url=f"https://spaceb.in/{response['payload']['id']}"
-            ),
-            InlineKeyboardButton(
-                text="View Raw",
-                url=f"https://spaceb.in/api/v1/documents/{response['payload']['id']}/raw",
-            ),
-        ]
-    ]
+
+    url = f"https://nekobin.com/{key}"
+
+    reply_text = f"Nekofied to *Nekobin* : {url}"
+
     message.reply_text(
-        text,
-        reply_markup=InlineKeyboardMarkup(buttons),
-        parse_mode=ParseMode.MARKDOWN,
-        disable_web_page_preview=True,
+        reply_text, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True
     )
 
 
-__mod_name__ = "Paste"
-
-PASTE_HANDLER = DisableAbleCommandHandler("npaste", paste, run_async=True)
-
+PASTE_HANDLER = DisableAbleCommandHandler("paste", paste, run_async=True)
 dispatcher.add_handler(PASTE_HANDLER)
+
+__command_list__ = ["paste"]
+__handlers__ = [PASTE_HANDLER]
