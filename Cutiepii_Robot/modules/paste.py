984ecbc24b6941a0f66ec9d5d02cad3c3f28128f
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-11-09 18:58:21 +0700

    Update paste.py

--- Cutiepii_Robot/modules/paste.py
+++ Cutiepii_Robot/modules/paste.py
@@ -10,6 +10,7 @@ from pyrogram.types import InlineKeyboardButton
 from Cutiepii_Robot import pgram, aiohttpsession
 from Cutiepii_Robot.utils.errors import capture_err
 from Cutiepii_Robot.utils.pastebin import paste
+from Cutiepii_Robot.utils.keyboard import ikb
 
 pattern = re.compile(
     r"^text/|json$|yaml$|xml$|toml$|x-sh$|x-shellscript$"
@@ -31,42 +32,55 @@ async def isPreviewUp(preview: str) -> bool:
     return False
 
 
+)
 @pgram.on_message(filters.command("paste") & ~filters.edited)
 @capture_err
-async def paste_func(_, message):
+async def paste_func(_, message: Message):
     if not message.reply_to_message:
-        return await message.reply_text(
-            "Reply To A Message With /paste"
+        return await eor(message, text="Reply To A Message With /paste")
+    r = message.reply_to_message
+
+    if not r.text and not r.document:
+        return await eor(
+            message, text="Only text and documents are supported."
         )
-    m = await message.reply_text("Pasting...")
-    if message.reply_to_message.text:
-        content = str(message.reply_to_message.text)
-    elif message.reply_to_message.document:
-        document = message.reply_to_message.document
-        if document.file_size > 1048576:
-            return await m.edit(
-                "You can only paste files smaller than 1MB."
-            )
-        if not pattern.search(document.mime_type):
+
+    m = await eor(message, text="Pasting...")
+
+    if r.text:
+        content = str(r.text)
+    elif r.document:
+        if r.document.file_size > 40000:
+            return await m.edit("You can only paste files smaller than 40KB.")
+
+        if not pattern.search(r.document.mime_type):
             return await m.edit("Only text files can be pasted.")
+
         doc = await message.reply_to_message.download()
+
         async with aiofiles.open(doc, mode="r") as f:
             content = await f.read()
-        os.remove(doc)
-    link = await paste(content)
-    preview = link + "/preview.png"
-    button = InlineKeyboard(row_width=1)
-    button.add(InlineKeyboardButton(text="Paste Link", url=link))
 
-    if await isPreviewUp(preview):
-        try:
+        os.remove(doc)
+
+    link = await paste(content)
+    kb = ikb({"Paste Link": link})
+    try:
+        if m.from_user.is_bot:
             await message.reply_photo(
-                photo=preview, quote=False, reply_markup=button
+                photo=link,
+                quote=False,
+                reply_markup=kb,
             )
-            return await m.delete()
-        except Exception:
-            pass
-    return await m.edit(link)
+        else:
+            await message.reply_photo(
+                photo=link,
+                quote=False,
+                caption=f"**Paste Link:** [Here]({link})",
+            )
+        await m.delete()
+    except Exception:
+        await m.edit("Here's your paste", reply_markup=kb)
 
 
 
