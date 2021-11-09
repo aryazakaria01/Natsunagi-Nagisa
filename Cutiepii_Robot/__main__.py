15f8cd4bcd3f8386bb4d207c85c31a4ef002490b
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-11-09 11:28:42 +0700

    Update __main__.py

--- Cutiepii_Robot/__main__.py
+++ Cutiepii_Robot/__main__.py
@@ -277,7 +277,7 @@ def start(update: Update, context: CallbackContext):
                 timeout=60,
             )
     else:
-        update.effective_message.reply_animation(
+        update.effective_message.reply_photo(
             GROUP_START_IMG, caption= "<b>Yes, Darling I'm alive!\nHaven't sleep since</b>: <code>{}</code>".format(
                 uptime
             ),
