09d34c96fb88db32e26fec1bedeb6c5281418d20
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-11-11 16:51:42 +0700

    Update music.py

--- Cutiepii_Robot/modules/music.py
+++ Cutiepii_Robot/modules/music.py
@@ -1,30 +1,4 @@
-"""
-MIT License
-
-Copyright (C) 2017-2019, Paul Larsen
-Copyright (C) 2021 Awesome-RJ
-Copyright (c) 2021, Yūki • Black Knights Union, <https://github.com/Awesome-RJ/CutiepiiRobot>
-
-This file is part of @Cutiepii_Robot (Telegram Bot)
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-
-furnished to do so, subject to the following conditions:
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
+from __future__ import unicode_literals
 
 import os
 import wget
@@ -33,6 +7,7 @@ import yt_dlp
 import asyncio
 import aiohttp
 import requests
+import ffmpeg
 
 from pyrogram import filters
 from pyrogram.types import Message
@@ -40,8 +15,58 @@ from Cutiepii_Robot import pgram, BOT_USERNAME
 from Cutiepii_Robot.utils.pluginhelp import get_text, progress
 from youtube_search import YoutubeSearch
 from youtubesearchpython import SearchVideos
+from asyncio import get_running_loop
+from functools import partial
+from io import BytesIO
+from urllib.parse import urlparse
+from Cutiepii_Robot import aiohttpsession as session
+from Cutiepii_Robot.modules.arq_client import arq
+from Cutiepii_Robot import pbot as app
+from Cutiepii_Robot.utils.errors import capture_err
+from Cutiepii_Robot.utils.pastebin import paste
+
+is_downloading = False
 
 
+def get_file_extension_from_url(url):
+    url_path = urlparse(url).path
+    basename = os.path.basename(url_path)
+    return basename.split(".")[-1]
+
+
+def download_youtube_audio(url: str):
+    global is_downloading
+    with youtube_dl.YoutubeDL(
+        {
+            "format": "bestaudio",
+            "writethumbnail": True,
+            "quiet": True,
+        }
+    ) as ydl:
+        info_dict = ydl.extract_info(url, download=False)
+        if int(float(info_dict["duration"])) > 600:
+            is_downloading = False
+            return []
+        ydl.process_info(info_dict)
+        audio_file = ydl.prepare_filename(info_dict)
+        basename = audio_file.rsplit(".", 1)[-2]
+        if info_dict["ext"] == "webm":
+            audio_file_opus = basename + ".opus"
+            ffmpeg.input(audio_file).output(
+                audio_file_opus, codec="copy", loglevel="error"
+            ).overwrite_output().run()
+            os.remove(audio_file)
+            audio_file = audio_file_opus
+        thumbnail_url = info_dict["thumbnail"]
+        thumbnail_file = (
+            basename
+            + "."
+            + get_file_extension_from_url(thumbnail_url)
+        )
+        title = info_dict["title"]
+        performer = info_dict["uploader"]
+        duration = int(float(info_dict["duration"]))
+    return [title, performer, duration, audio_file, thumbnail_file]
 
 def time_to_seconds(time):
     stringt = str(time)
@@ -165,3 +190,103 @@ def song(client, message):
         os.remove(thumb_name)
     except Exception as e:
         print(e)
+
+@app.on_message(filters.command("ytmusic"))
+@capture_err
+async def music(_, message):
+    global is_downloading
+    if len(message.command) != 2:
+        return await message.reply_text(
+            "/ytmusic needs a link as argument"
+        )
+    url = message.text.split(None, 1)[1]
+    if is_downloading:
+        return await message.reply_text(
+            "Another download is in progress, try again after sometime."
+        )
+    is_downloading = True
+    m = await message.reply_text(
+        f"Downloading {url}", disable_web_page_preview=True
+    )
+    try:
+        loop = get_running_loop()
+        music = await loop.run_in_executor(
+            None, partial(download_youtube_audio, url)
+        )
+        if not music:
+            await m.edit("Too Long, Can't Download.")
+        (
+            title,
+            performer,
+            duration,
+            audio_file,
+            thumbnail_file,
+        ) = music
+    except Exception as e:
+        is_downloading = False
+        return await m.edit(str(e))
+    await message.reply_audio(
+        audio_file,
+        duration=duration,
+        performer=performer,
+        title=title,
+        thumb=thumbnail_file,
+    )
+    await m.delete()
+    os.remove(audio_file)
+    os.remove(thumbnail_file)
+    is_downloading = False
+
+
+# Funtion To Download Song
+async def download_song(url):
+    async with session.get(url) as resp:
+        song = await resp.read()
+    song = BytesIO(song)
+    song.name = "a.mp3"
+    return song
+
+
+# Jiosaavn Music
+
+
+@app.on_message(filters.command("saavn") & ~filters.edited)
+@capture_err
+async def jssong(_, message):
+    global is_downloading
+    if len(message.command) < 2:
+        return await message.reply_text(
+            "/saavn requires an argument."
+        )
+    if is_downloading:
+        return await message.reply_text(
+            "Another download is in progress, try again after sometime."
+        )
+    is_downloading = True
+    text = message.text.split(None, 1)[1]
+    m = await message.reply_text("Searching...")
+    try:
+        songs = await arq.saavn(text)
+        if not songs.ok:
+            await m.edit(songs.result)
+            is_downloading = False
+            return
+        sname = songs.result[0].song
+        slink = songs.result[0].media_url
+        ssingers = songs.result[0].singers
+        sduration = songs.result[0].duration
+        await m.edit("Downloading")
+        song = await download_song(slink)
+        await m.edit("Uploading")
+        await message.reply_audio(
+            audio=song,
+            title=sname,
+            performer=ssingers,
+            duration=sduration,
+        )
+        await m.delete()
+    except Exception as e:
+        is_downloading = False
+        return await m.edit(str(e))
+    is_downloading = False
+    song.close()
