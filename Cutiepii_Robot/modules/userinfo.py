a815c64fe1307a0988d6e9e163ae0b1e408a21a0
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-11-11 22:18:50 +0700

    Update userinfo.py

--- Cutiepii_Robot/modules/userinfo.py
+++ Cutiepii_Robot/modules/userinfo.py
@@ -298,25 +298,25 @@ def info(update: Update, context: CallbackContext):
     disaster_level_present = False
 
     if user.id == OWNER_ID:
-        text += "\n\nThe Disaster level of this person is Alpha."
+        text += "\n\nThe Disaster level of this person is The Detective."
         disaster_level_present = True
     elif user.id in DEV_USERS:
-        text += "\n\nThis user is member of Legends."
+        text += "\n\nThis user is member of Assistant Of The Detective."
         disaster_level_present = True
     elif user.id in DRAGONS:
-        text += "\n\nThe Disaster level of this person is Ultimate."
+        text += "\n\nThe Disaster level of this person is Friend Of Detective."
         disaster_level_present = True
     elif user.id in DEMONS:
-        text += "\n\nThe Disaster level of this person is Beta."
+        text += "\n\nThe Disaster level of this person is Supporter Of Defender."
         disaster_level_present = True
     elif user.id in TIGERS:
-        text += "\n\nThe Disaster level of this person is Champion."
+        text += "\n\nThe Disaster level of this person is Defender Of The Detective ."
         disaster_level_present = True
     elif user.id in WOLVES:
-        text += "\n\nThe Disaster level of this person is Rookie."
+        text += "\n\nThe Disaster level of this person is Second Defender Of The Detective ."
         disaster_level_present = True
     elif user.id == 1482952149:
-         text += "\n\nCo-Owner Of A Bot. Queen Of @Badboyanim. Bot Name Inspired From 'Tantei Wa Mou'."
+         text += "\n\nCo-Owner Of A Bot. Queen Of @Badboyanim. Bot Name Inspired From Tantei Wa Mou, Shindeiru."
          disaster_level_present = True
 
     if disaster_level_present:
