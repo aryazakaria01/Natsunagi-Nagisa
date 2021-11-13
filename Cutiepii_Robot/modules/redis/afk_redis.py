9fb1aa1c4ee27024acf22112226cf0a2c4747989
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-11-13 14:17:09 +0700

    ok

--- Cutiepii_Robot/modules/redis/afk_redis.py
+++ Cutiepii_Robot/modules/redis/afk_redis.py
@@ -1,38 +1,8 @@
-"""
-MIT License
-
-Copyright (C) 2017-2019, Paul Larsen
-Copyright (C) 2021 Awesome-RJ
-Copyright (c) 2021, Yūki • Black Knights Union, <https://github.com/Awesome-RJ/CutiepiiRobot>
-
-This file is part of @Cutiepii_Robot (Telegram Bot)
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-
-furnished to do so, subject to the following conditions:
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
-
 from Cutiepii_Robot import REDIS
 
-# AFK
 def is_user_afk(userid):
     rget = REDIS.get(f'is_afk_{userid}')
     return bool(rget)
-
 
 def start_afk(userid, reason):
     REDIS.set(f'is_afk_{userid}', reason)
@@ -44,6 +14,5 @@ def end_afk(userid):
     REDIS.delete(f'is_afk_{userid}')
     return True
 
-# Helpers
 def strb(redis_string):
     return str(redis_string)
