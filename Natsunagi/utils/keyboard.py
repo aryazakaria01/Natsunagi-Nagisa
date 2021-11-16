a6e62929cc726840ff4b08a6d22b301a0579c3c4
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-11-16 10:00:48 +0700

    Update keyboard.py

--- Natsunagi/utils/keyboard.py
+++ Natsunagi/utils/keyboard.py
@@ -1,7 +1,7 @@
 from pykeyboard import InlineKeyboard
 from pyrogram.types import InlineKeyboardButton as Ikb
 
-from Cutiepii_Robot.utils.functions import get_urls_from_text as is_url
+from Natsunagi.utils.functions import get_urls_from_text as is_url
 
 
 def keyboard(buttons_list, row_width: int = 2):
