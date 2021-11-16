bd68406f8602050db469423d1b0acd6ae638dcda
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-11-16 09:52:40 +0700

    Update fetch.py

--- Natsunagi/utils/fetch.py
+++ Natsunagi/utils/fetch.py
@@ -26,7 +26,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-from Cutiepii_Robot import aiohttpsession
+from Natsunagi import aiohttpsession
 
 headers = {
     "Accept": "application/json",
