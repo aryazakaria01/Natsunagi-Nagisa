e8b0e253e4748b90a0e3e4e722eec873a509be0d
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-11-17 09:28:25 +0700

    blok

--- Natsunagi/modules/tiny.py
+++ Natsunagi/modules/tiny.py
@@ -13,7 +13,7 @@ async def _(event):
            return
     kontol = await event.reply("`Processing tiny...`")
     ik = await tbot.download_media(reply)
-    im1 = Image.open("Natsunagi/images/ken.png")
+    im1 = Image.open("Natsunagi/image/ken.png")
     if ik.endswith(".tgs"):
         await tbot.download_media(reply, "ken.tgs")
         os.system("lottie_convert.py ken.tgs json.json")
