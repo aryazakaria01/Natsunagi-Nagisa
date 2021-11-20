6385894bcb8178ab905cd68c1015c899389c867d
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-11-20 20:20:04 +0700

    Fix batbin that doesn't work

--- Natsunagi/utils/pastebin.py
+++ Natsunagi/utils/pastebin.py
@@ -4,7 +4,7 @@ BASE = "https://batbin.me/"
 
 
 async def paste(content: str):
-    resp = await post(f"{BASE}api/paste", data={"content": content})
-    if not resp:
+    resp = await post(f"{BASE}api/v2/paste", data=content)
+    if not resp["success"]:
         return
     return BASE + resp["message"]
