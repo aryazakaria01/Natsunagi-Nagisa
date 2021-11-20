9735940b2251574b493c7163150cf7c7452aa811
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-11-20 16:21:49 +0700

    f

--- Natsunagi/conf.py
+++ Natsunagi/conf.py
@@ -1,10 +1,64 @@
+import os
+import sys
+
+import yaml
 from envparse import env
 from Natsunagi import LOGGER
 
 DEFAULTS = {
     "LOAD_MODULES": True,
+    "DEBUG_MODE": True,
+    "REDIS_HOST": "localhost",
+    "REDIS_PORT": 6379,
+    "REDIS_DB_FSM": 1,
+    "MONGODB_URI": "localhost",
+    "MONGO_DB": "DaisyX",
+    "API_PORT": 8080,
+    "JOIN_CONFIRM_DURATION": "30m",
 }
 
+CONFIG_PATH = "data/bot_conf.yaml"
+if os.name == "nt":
+    log.debug("Detected Windows, changing config path...")
+    CONFIG_PATH = os.getcwd() + "\\data\\bot_conf.yaml"
+
+if os.path.isfile(CONFIG_PATH):
+    log.info(CONFIG_PATH)
+    for item in (
+        data := yaml.load(open("data/bot_conf.yaml", "r"), Loader=yaml.CLoader)
+    ):
+        DEFAULTS[item.upper()] = data[item]
+else:
+    log.info("Using env vars")
+
+def get_list_key(name, required=False):
+    if name in DEFAULTS:
+        default = DEFAULTS[name]
+    else:
+        default = None
+    if not (data := env.list(name, default=default)) and not required:
+        log.warn("No list key: " + name)
+        return []
+    elif not data:
+        log.critical("No list key: " + name)
+        sys.exit(2)
+    else:
+        return data
+
+def get_bool_key(name, required=False):
+    if name in DEFAULTS:
+        default = DEFAULTS[name]
+    else:
+        default = None
+    if not (data := env.bool(name, default=default)) and not required:
+        log.warn("No bool key: " + name)
+        return False
+    elif not data:
+        log.critical("No bool key: " + name)
+        sys.exit(2)
+    else:
+        return data   
+ 
 def get_str_key(name, required=False):
     if name in DEFAULTS:
         default = DEFAULTS[name]
