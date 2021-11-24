511475f91b815911063608a7ca6e2c7409a55ee7
Yoga Pranata <45356875+zYxDevs@users.noreply.github.com>
2021-11-24 09:49:08 +0700

    kelupaan om

--- Natsunagi/__main__.py
+++ Natsunagi/__main__.py
@@ -438,7 +438,7 @@ def natsunagi_about_callback(update, context):
         query.message.edit_text(
                 PM_START_TEXT.format(
                     escape_markdown(context.bot.first_name),
-                    escape_markdown(first_name),
+                    escape_markdown(update.effective_user.first_name),
                     escape_markdown(uptime),
                     sql.num_users(),
                     sql.num_chats()),
