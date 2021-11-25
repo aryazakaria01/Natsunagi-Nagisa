e8dcb1fc2e48f22fca264bc4dbaa22e44a74e7d0
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-11-25 10:56:45 +0700

    ok

--- Natsunagi/utils/pastebin.py
+++ Natsunagi/utils/pastebin.py
@@ -6,7 +6,7 @@ from Natsunagi.utils.http import post
 BASE = "https://batbin.me/"
 
 
-async def paste(content: str):
+async def hpaste(content: str):
     resp = await post(f"{BASE}api/v2/paste", data=content)
     if not resp["success"]:
         return
