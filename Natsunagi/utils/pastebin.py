6aae658894d8819bbc0c351121ead8870c7f3015
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-11-25 10:35:10 +0700

    pastebin: added loop

--- Natsunagi/utils/pastebin.py
+++ Natsunagi/utils/pastebin.py
@@ -1,3 +1,6 @@
+import socket
+from asyncio import get_running_loop
+from functools import partial
 from Natsunagi.utils.http import post
 
 BASE = "https://batbin.me/"
@@ -8,3 +11,24 @@ async def paste(content: str):
     if not resp["success"]:
         return
     return BASE + resp["message"]
+
+
+def _netcat(host, port, content):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+    s.sendall(content.encode())
+    s.shutdown(socket.SHUT_WR)
+    while True:
+        data = s.recv(4096).decode("utf-8").strip("\n\x00")
+        if not data:
+            break
+        return data
+    s.close()
+
+
+async def paste(content):
+    loop = get_running_loop()
+    link = await loop.run_in_executor(
+        None, partial(_netcat, "ezup.dev", 9999, content)
+    )
+    return link
