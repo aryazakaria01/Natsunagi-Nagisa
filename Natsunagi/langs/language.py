94e1811b66891ad5637c1d84c32456ae3a1de98c
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-11-26 21:17:22 +0700

    Hadeh

--- Natsunagi/langs/language.py
+++ Natsunagi/langs/language.py
@@ -19,11 +19,11 @@ class Language:
             return en_string
 
     def reload_strings(self) -> None:
-        for filename in os.listdir(r"./YuiiDev/langs"):
+        for filename in os.listdir(r"./Natsunagi/langs"):
             if filename.endswith(".yaml"):
                 language_name = filename[:-5]
                 self.languages[language_name] = yaml.safe_load(
-                    open(r"./YuiiDev/langs/" + filename, encoding="utf8")
+                    open(r"./Natsunagi/langs/" + filename, encoding="utf8")
                 )
 
     def get_languages(self) -> Dict:
