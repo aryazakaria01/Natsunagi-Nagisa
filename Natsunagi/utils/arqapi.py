ae1ebe2ea2b83412326c63d0309403a26e75e7e6
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-11-26 08:48:05 +0700

    Imports from same package are not grouped

--- Natsunagi/utils/arqapi.py
+++ Natsunagi/utils/arqapi.py
@@ -4,15 +4,12 @@ from random import randint
 from time import time
 
 import aiohttp
-from Natsunagi import aiohttpsession 
+from Natsunagi import ARQ_API_KEY, ARQ_API_URL, BOT_ID, OWNER_ID, aiohttpsession, pbot
 from aiohttp import ClientSession
 
 from google_trans_new import google_translator
 from Python_ARQ import ARQ
 from search_engine_parser import GoogleSearch
-
-from Natsunagi import BOT_ID, OWNER_ID, ARQ_API_URL, ARQ_API_KEY
-from Natsunagi import pbot
 
 ARQ_API = "WZQUBA-PFAZQJ-OMIINH-MIVHYM-ARQ"
 ARQ_API_KEY = "WZQUBA-PFAZQJ-OMIINH-MIVHYM-ARQ"
