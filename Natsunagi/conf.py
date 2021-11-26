6ec387136b4aa2faf9e523844f556b461a30703c
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-11-26 11:06:30 +0700

    Unnecessary `else` / `elif` used after `return`

--- Natsunagi/conf.py
+++ Natsunagi/conf.py
@@ -37,7 +37,7 @@ def get_list_key(name, required=False):
     if not (data := env.list(name, default=default)) and not required:
         LOGGER.warn("No list key: " + name)
         return []
-    elif not data:
+    if not data:
         LOGGER.critical("No list key: " + name)
         sys.exit(2)
     else:
@@ -49,7 +49,7 @@ def get_bool_key(name, required=False):
     if not (data := env.bool(name, default=default)) and not required:
         LOGGER.warn("No bool key: " + name)
         return False
-    elif not data:
+    if not data:
         LOGGER.critical("No bool key: " + name)
         sys.exit(2)
     else:
@@ -61,7 +61,7 @@ def get_str_key(name, required=False):
     if not (data := env.str(name, default=default)) and not required:
         LOGGER.warn("No str key: " + name)
         return None
-    elif not data:
+    if not data:
         LOGGER.critical("No str key: " + name)
         sys.exit(2)
     else:
@@ -73,7 +73,7 @@ def get_int_key(name, required=False):
     if not (data := env.int(name, default=default)) and not required:
         LOGGER.warn("No int key: " + name)
         return None
-    elif not data:
+    if not data:
         LOGGER.critical("No int key: " + name)
         sys.exit(2)
     else:
