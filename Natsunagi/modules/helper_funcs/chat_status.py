d4fcdbfe88b12651c26c946357ca0419c39d6f0d
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-11-26 08:30:57 +0700

    Imports from same package are not grouped

--- Natsunagi/modules/helper_funcs/chat_status.py
+++ Natsunagi/modules/helper_funcs/chat_status.py
@@ -1,26 +1,3 @@
-"""
-MIT License
-Copyright (C) 2017-2019, Paul Larsen
-Copyright (C) 2021 Awesome-RJ
-Copyright (c) 2021, Yūki • Black Knights Union, <https://github.com/Awesome-RJ/CutiepiiRobot>
-This file is part of @Cutiepii_Robot (Telegram Bot)
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
-
 from time import perf_counter
 from functools import wraps
 from cachetools import TTLCache
@@ -35,6 +12,7 @@ from Natsunagi import (
     WOLVES,
     dispatcher,
 )
+from Natsunagi.modules import connection
 
 from telegram import Chat, ChatMember, ParseMode, Update, User
 from telegram.ext import CallbackContext
@@ -462,7 +440,5 @@ def connection_status(func):
 
     return connected_status
 
-
-from Natsunagi.modules import connection
 
 connected = connection.connected
