a46e45f2294cf005612d39080be1174344d330ff
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-11-26 08:57:05 +0700

    Multiple imports for an import name detected

--- Natsunagi/modules/admin.py
+++ Natsunagi/modules/admin.py
@@ -7,7 +7,6 @@ from telegram.ext import CallbackContext, CommandHandler, Filters, run_async
 from telegram.utils.helpers import mention_html
 from telethon import events
 from telethon.tl import functions, types
-from telethon import events
 from telethon.errors import *
 from telethon.tl import *
 from telethon import *
