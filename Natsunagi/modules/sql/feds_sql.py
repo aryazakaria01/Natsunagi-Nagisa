80a5026d0ec527897e1c0556c3fa53c6ce40cf72
『TØNIC』 乂 ₭ILLΣR <57848595+Tonic990@users.noreply.github.com>
2021-12-01 22:38:59 +0700

    Update feds_sql.py

--- Natsunagi/modules/sql/feds_sql.py
+++ Natsunagi/modules/sql/feds_sql.py
@@ -3,7 +3,7 @@ import threading
 
 from Natsunagi import dispatcher
 from Natsunagi.modules.sql import BASE, SESSION
-from sqlalchemy import Boolean, Column, Integer, String, UnicodeText
+from sqlalchemy import Boolean, Column, BigInteger, String, UnicodeText
 from telegram.error import BadRequest, Unauthorized
 
 
@@ -45,7 +45,7 @@ class BansF(BASE):
     last_name = Column(UnicodeText)
     user_name = Column(UnicodeText)
     reason = Column(UnicodeText, default="")
-    time = Column(Integer, default=0)
+    time = Column(BigInteger, default=0)
 
     def __init__(self, fed_id, user_id, first_name, last_name, user_name, reason, time):
         self.fed_id = fed_id
@@ -59,7 +59,7 @@ class BansF(BASE):
 
 class FedsUserSettings(BASE):
     __tablename__ = "feds_settings"
-    user_id = Column(Integer, primary_key=True)
+    user_id = Column(BigInteger, primary_key=True)
     should_report = Column(Boolean, default=True)
 
     def __init__(self, user_id):
