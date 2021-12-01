546e92079fae0c06b57c5a586ec4cb80ade7829b
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-12-01 11:51:58 +0700

    Update adminpermissions.py

--- Natsunagi/utils/adminpermissions.py
+++ Natsunagi/utils/adminpermissions.py
@@ -1,7 +1,7 @@
 import asyncio
 from pyrogram import filters
 from pyrogram.types import ChatPermissions, Message
-from Natsunagi import pbot as app
+from Natsunagi import pgram as app
 
 
 async def member_permissions(chat_id: int, user_id: int):
