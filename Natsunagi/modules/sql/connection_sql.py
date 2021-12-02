04d789c7f651d48878fb6891e6bff96c9b72ccbf
UserLazy <78595134+UserLazy@users.noreply.github.com>
2021-12-03 06:34:39 +0700

    Update connection_sql.py

--- Natsunagi/modules/sql/connection_sql.py
+++ Natsunagi/modules/sql/connection_sql.py
@@ -2,7 +2,8 @@ import threading
 import time
 from typing import Union
 
-from sqlalchemy import Column, String, Boolean, UnicodeText, BigInteger, Integer
+from sqlalchemy import Column, String, Boolean, UnicodeText, Integer
+from sqlalchemy.sql.sqltypes import BigInteger
 
 from Natsunagi.modules.sql import SESSION, BASE
 
