31a610e6502cf4bdd9418480b83e37431f8a971c
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-12-06 21:36:44 +0700

    Add dogbin and pasty lus

--- Natsunagi/utils/pastebin.py
+++ Natsunagi/utils/pastebin.py
@@ -1,7 +1,17 @@
 import socket
+import json
+import request
+
 from asyncio import get_running_loop
 from functools import partial
 from Natsunagi.utils.http import post
+from Natsunagi import Config
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36",
+    "content-type": "application/json",
+}
+
 
 BASE = "https://batbin.me/"
 
@@ -32,3 +42,78 @@ async def epaste(content):
         None, partial(_netcat, "ezup.dev", 9999, content)
     )
     return link
+
+
+"""
+async def p_paste(message, extension=None):
+    """
+    To Paste the given message/text/code to paste.pelkum.dev
+    """
+    siteurl = "https://pasty.lus.pm/api/v1/pastes"
+    data = {"content": message}
+    try:
+        response = requests.post(url=siteurl, data=json.dumps(data), headers=headers)
+    except Exception as e:
+        return {"error": str(e)}
+    if response.ok:
+        response = response.json()
+        purl = (
+            f"https://pasty.lus.pm/{response['id']}.{extension}"
+            if extension
+            else f"https://pasty.lus.pm/{response['id']}.txt"
+        )
+        try:
+            from Natsunagi import pgram
+
+            await pgram.send_message(
+                Config.BOTLOG_CHATID,
+                f"**You have created a new paste in pasty bin.** Link to pasty is [here]({purl}). You can delete that paste by using this token `{response['deletionToken']}`",
+            )
+        except Exception as e:
+            LOGS.info(str(e))
+        return {
+            "url": purl,
+            "raw": f"https://pasty.lus.pm/{response['id']}/raw",
+            "bin": "Pasty",
+        }
+    return {"error": "Unable to reach pasty.lus.pm"}
+
+
+async def d_paste(message, extension=None):
+    """
+    To Paste the given message/text/code to dogbin
+    """
+    siteurl = "http://catbin.up.railway.app/documents"
+    data = {"content": message}
+    try:
+        response = requests.post(url=siteurl, data=json.dumps(data), headers=headers)
+    except Exception as e:
+        return {"error": str(e)}
+    if response.ok:
+        response = response.json()
+        purl = (
+            f"http://catbin.up.railway.app/{response['key']}.{extension}"
+            if extension
+            else f"http://catbin.up.railway.app/{response['key']}"
+        )
+        return {
+            "url": purl,
+            "raw": f"http://catbin.up.railway.app/raw/{response['key']}",
+            "bin": "Dog",
+        }
+    return {"error": "Unable to reach dogbin."}
+
+
+async def pastetext(text_to_print, pastetype=None, extension=None):
+    response = {"error": "something went wrong"}
+    if pastetype is not None:
+        if pastetype == "p":
+            response = await p_paste(text_to_print, extension)
+        elif pastetype == "d":
+            response = await d_paste(text_to_print, extension)
+    if "error" in response:
+        response = await p_paste(text_to_print, extension)
+    if "error" in response:
+        response = await d_paste(text_to_print, extension)
+    return response
+"""
