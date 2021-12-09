9daaf23e9037167a28bd83557f8164c1a84bf67f
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-12-09 16:00:29 +0700

    userinfo: refactor

--- Natsunagi/modules/userinfo.py
+++ Natsunagi/modules/userinfo.py
@@ -499,6 +499,43 @@ def about_bio(update: Update, context: CallbackContext):
         update.effective_message.reply_text(
             "You haven't had a bio set about yourself yet!",
         )
+    message = update.effective_message
+    if message.reply_to_message:
+        repl_message = message.reply_to_message
+        user_id = repl_message.from_user.id
+        if user_id == message.from_user.id:
+            message.reply_text(
+                "Ha, you can't set your own bio! You're at the mercy of others here..."
+            )
+            return
+        sender_id = update.effective_user.id
+
+        if (
+                user_id == bot.id
+                and sender_id not in SUDO_USERS
+                and sender_id not in DEV_USERS
+        ):
+            message.reply_text(
+                "Erm... yeah, I only trust sudo users or developers to set my bio."
+            )
+            return
+        text = message.text
+        # use python's maxsplit to only remove the cmd, hence keeping newlines.
+        bio = text.split(None, 1)
+        if len(bio) == 2:
+            if len(bio[1]) < MAX_MESSAGE_LENGTH // 4:
+                sql.set_user_bio(user_id, bio[1])
+                message.reply_text(
+                    "Updated {}'s bio!".format(repl_message.from_user.first_name)
+                )
+            else:
+                message.reply_text(
+                    "A bio needs to be under {} characters! You tried to set {}.".format(
+                        MAX_MESSAGE_LENGTH // 4, len(bio[1])
+                    )
+                )
+    else:
+        message.reply_text("Reply to someone's message to set their bio!")
 
 
 @natsunagicmd(command="setbio")
