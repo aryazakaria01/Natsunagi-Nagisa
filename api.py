9ca82b5a1ea4cb6223dcffdb9fa6f5607b910eed
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-12-10 11:21:31 +0700

    [NEW] Natsunagi api, using FastAPI by tiangolo

---
+++ api.py
@@ -0,0 +1,35 @@
+import Natsunagi.modules.sql.global_bans_sql as sql1
+import Natsunagi.modules.sql.blacklistusers_sql as sql2
+
+from fastapi import FastAPI
+from telegram import __version__ as v
+
+Natsunagi = FastAPI()
+
+@Natsunagi.get("/")
+def read_root():
+    return {"status": "online", "ptb_ver": v }
+
+
+@Natsunagi.get("/getuser/{user_id}")
+def read_item(user_id: int):
+    try:
+        a = sql1.is_user_gbanned(user_id)
+        if a:
+            user = sql1.get_gbanned_user(user_id)
+            areason = user.reason
+        else:
+            areason = None
+
+        b = sql2.is_user_blacklisted(user_id)
+        if b:
+            breason = sql2.get_reason(user_id)
+        else:
+            breason = None
+        return {"status": "ok", "user_id": user_id, "gbanned": a, "gban_reason" : areason, "blacklisted" : b, "blacklist_reason" : breason}
+    except Exception:
+        a = None
+        areason = None
+        b = None
+        breason = None
+        return {"status": "ok", "user_id": user_id, "gbanned": a, "gban_reason" : areason, "blacklisted" : b, "blacklist_reason" : breason}
