2a9c9180f4a9de206beeab726aa3b4a112e2b744
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-12-10 20:09:51 +0700

    Ok

---
+++ Natsunagi/modules/sql/acm_sql.py
@@ -0,0 +1,36 @@
+import threading
+
+from sqlalchemy import Column, String, Boolean
+
+from Natsunagi.modules.sql import SESSION, BASE
+
+class CleanLinked(BASE):
+    __tablename__ = "clean_linked"
+    chat_id = Column(String(14), primary_key=True)
+    status = Column(Boolean, default=False)
+
+    def __init__(self, chat_id, status):
+        self.chat_id = str(chat_id)
+        self.status = status
+
+CleanLinked.__table__.create(checkfirst=True)
+
+CLEANLINKED_LOCK = threading.RLock()
+
+def getCleanLinked(chat_id):
+    try:
+        resultObj = SESSION.query(CleanLinked).get(str(chat_id))
+        if resultObj:
+            return resultObj.status
+        return False #default
+    finally:
+        SESSION.close()
+
+def setCleanLinked(chat_id, status):
+    with CLEANLINKED_LOCK:
+        prevObj = SESSION.query(CleanLinked).get(str(chat_id))
+        if prevObj:
+            SESSION.delete(prevObj)
+        newObj = CleanLinked(str(chat_id), status)
+        SESSION.add(newObj)
+        SESSION.commit()
