07bfb243bc5a2d663682c6a8742a6d2d72abe24a
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-12-11 13:02:55 +0700

    Redefining built-in 'set'

--- Natsunagi/pyrogramee/pluginshelper.py
+++ Natsunagi/pyrogramee/pluginshelper.py
@@ -257,13 +257,6 @@ def get_text(message: Message) -> [None, str]:
 admins: Dict[str, List[User]] = {}
 
 
-def set(chat_id: Union[str, int], admins_: List[User]):
-    if isinstance(chat_id, int):
-        chat_id = str(chat_id)
-
-    admins[chat_id] = admins_
-
-
 def get(chat_id: Union[str, int]) -> Union[List[User], bool]:
     if isinstance(chat_id, int):
         chat_id = str(chat_id)
