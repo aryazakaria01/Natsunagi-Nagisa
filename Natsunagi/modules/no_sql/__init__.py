98f34570dc4832515495bb4611e9ce5e5c0132f7
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-12-13 22:41:34 +0700

    init: fixed

--- Natsunagi/modules/no_sql/__init__.py
+++ Natsunagi/modules/no_sql/__init__.py
@@ -5,11 +5,11 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from odmantic import AIOEngine
 from pymongo import MongoClient, collection
 
-from Natsunagi import MONGO_URI, MONGO_DB, MONGO_PORT, MONGO_DB_URL
+from Natsunagi import MONGO_URI, MONGO_DB, MONGO_PORT
 
 # MongoDB Client
-mongodb = MongoClient(MONGO_DB_URL, 27017)[MONGO_DB]
-motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URL)
+mongodb = MongoClient(MONGO_URI, 27017)[MONGO_DB]
+motor = motor_asyncio.AsyncIOMotorClient(MONGO_URI)
 db = motor[MONGO_DB]
 engine = AIOEngine(motor, MONGO_DB)
 DB_CLIENT = MongoClient(MONGO_URI)
