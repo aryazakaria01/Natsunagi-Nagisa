fdbe3278ea3649b6210b2fb8e6ecc8f94af196e1
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-12-18 14:28:08 +0700

    global_bans: fixed

--- Natsunagi/modules/global_bans.py
+++ Natsunagi/modules/global_bans.py
@@ -477,7 +477,7 @@ def enforce_gban(update: Update, context: CallbackContext):
 
         if msg.reply_to_message:
             user = msg.reply_to_message.from_user
-            if user and not is_user_admin(chat, user.id):
+            if user and not is_user_admin(update, user.id):
                 check_and_ban(update, user.id, should_message=False)
 
 
@@ -542,13 +542,13 @@ def __chat_settings__(chat_id, user_id):
 
 __help__ = f"""
 *Admins only:*
-  ➢ `/antispam <on/off/yes/no>`*:* Will toggle our antispam tech or return your current settings.
+❂ `/antispam <on/off/yes/no>`*:* Will toggle our antispam tech or return your current settings.
+
 Anti-Spam, used by bot devs to ban spammers across all groups. This helps protect \
 you and your groups by removing spam flooders as quickly as possible.
-
 *Note:* Users can appeal gbans or report spammers at @{SUPPORT_CHAT}
-This also integrates @Spamwatch API to remove Spammers as much as possible from your chatroom!
 
+This also integrates @Spamwatch API to remove Spammers as much as possible from your chatroom!
 *What is SpamWatch?*
 SpamWatch maintains a large constantly updated ban-list of spambots, trolls, bitcoin spammers and unsavoury characters[.](https://telegra.ph/file/f584b643c6f4be0b1de53.jpg)
 Constantly help banning spammers off from your group automatically So, you wont have to worry about spammers storming your group.
