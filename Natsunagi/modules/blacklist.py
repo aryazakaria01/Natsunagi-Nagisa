f34f78441a006ce6ac91f0f807ae5f1f03eec5f1
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-12-20 11:13:19 +0700

    ok

--- Natsunagi/modules/blacklist.py
+++ Natsunagi/modules/blacklist.py
@@ -242,7 +242,6 @@ def blacklist_mode(update, context):
         elif args[0].lower() == "tban":
             if len(args) == 1:
                 teks = """It looks like you tried to set time value for blacklist but you didn't specified time; Try, `/blacklistmode tban <timevalue>`.
-				
     Examples of time value: 4m = 4 minutes, 3h = 3 hours, 6d = 6 days, 5w = 5 weeks."""
                 send_message(update.effective_message, teks, parse_mode="markdown")
                 return ""
@@ -443,20 +442,26 @@ __mod_name__ = "Blacklists"
 
 __help__ = """
 Blacklists are used to stop certain triggers from being said in a group. Any time the trigger is mentioned, the message will immediately be deleted. A good combo is sometimes to pair this up with warn filters!
+
 NOTE: Blacklists do not affect group admins.
   ➢ `/blacklist`*:* View the current blacklisted words.
+
 Admins only:
   ➢ `/addblacklist <triggers>`*:* Add a trigger to the blacklist. Each line is considered one trigger, so using different lines will allow you to add multiple triggers.
   ➢ `/unblacklist <triggers>`*:* Remove triggers from the blacklist. Same newline logic applies here, so you can remove multiple triggers at once.
   ➢ `/blacklistmode <off/del/warn/ban/kick/mute/tban/tmute>`*:* Action to perform when someone sends blacklisted words.
 Blacklist sticker is used to stop certain stickers. Whenever a sticker is sent, the message will be deleted immediately.
+
 NOTE: Blacklist stickers do not affect the group admin
+
   ➢ `/blsticker`*:* See current blacklisted sticker
+
 Only admin:
   ➢ `/addblsticker <sticker link>`*:* Add the sticker trigger to the black list. Can be added via reply sticker
   ➢ `/unblsticker <sticker link>`*:* Remove triggers from blacklist. The same newline logic applies here, so you can delete multiple triggers at once
   ➢ `/rmblsticker <sticker link>`*:* Same as above
   ➢ `/blstickermode <delete/ban/tban/mute/tmute>`*:* sets up a default action on what to do if users use blacklisted stickers
+
 Note:
   <sticker link> can be https://t.me/addstickers/<stickerpackname> or just <sticker> or reply to the sticker message
 """
