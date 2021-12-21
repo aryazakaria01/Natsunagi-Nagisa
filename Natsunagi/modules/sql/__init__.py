6b6527f767593846c47c70779510926f7e991bb5
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-12-21 15:34:57 +0700

    sql --> init: add logger and make the `autoflush=True`

--- Natsunagi/modules/sql/__init__.py
+++ Natsunagi/modules/sql/__init__.py
@@ -1,16 +1,25 @@
+import sys
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
 
-from Natsunagi import DB_URL
+from Natsunagi import DB_URL, LOGGER
 
 
 def start() -> scoped_session:
     engine = create_engine(DB_URL, client_encoding="utf8")
+    LOGGER.info("PostgreSQL Connecting to database......")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=True))
 
 
 BASE = declarative_base()
-SESSION: scoped_session = start()
+try:
+    SESSION: scoped_session = start()
+except Exception as e:
+    LOGGER.exception(f"PostgreSQL Failed to connect due to {e}")
+    sys.exit()
+
+LOGGER.info("PostgreSQL Connection successful, session started.")
