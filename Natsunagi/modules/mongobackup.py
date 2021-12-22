40dd9aefb739536c2d9dc15a613dcf7b15cc85c7
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-12-22 08:32:31 +0700

    database backup feature

---
+++ Natsunagi/modules/mongobackup.py
@@ -0,0 +1,32 @@
+from os import remove
+from os import system as execute
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from Natsunagi import MONGO_URI, DEV_USERS, pgram as app
+
+
+@app.on_message(filters.command("backup") & filters.user(DEV_USERS))
+async def backup(_, message: Message):
+    if message.chat.type != "private":
+        return await message.reply("This command can only be used in private")
+
+    m = await message.reply("Backing up data...")
+
+    code = execute(f'mongodump --uri "{MONGO_URI}"')
+    if int(code) != 0:
+        return await m.edit(
+            "Looks like you don't have mongo-database-tools installed "
+            + "grab it from mongodb.com/try/download/database-tools"
+        )
+
+    code = execute("zip backup.zip -r9 dump/*")
+    if int(code) != 0:
+        return await m.edit(
+            "Looks like you don't have `zip` package installed, BACKUP FAILED!"
+        )
+
+    await message.reply_document("backup.zip")
+    await m.delete()
+    remove("backup.zip")
