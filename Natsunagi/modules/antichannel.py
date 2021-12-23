96325742500c798ac2d1c809e6dc77bedd40ebed
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-12-23 09:08:56 +0700

    miss

--- Natsunagi/modules/antichannel.py
+++ Natsunagi/modules/antichannel.py
@@ -1,7 +1,7 @@
 import html
 
 import requests
-from telegram import TelegramError, Update
+from telegram import TelegramError, Update, ParseMode
 from telegram.ext import CallbackContext, CommandHandler, MessageHandler
 from telegram.ext.filters import Filters
 
