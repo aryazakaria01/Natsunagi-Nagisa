19cb7df216970835f5df9970696767935a74fb3e
aryazakaria01 <80420642+aryazakaria01@users.noreply.github.com>
2021-12-25 02:08:09 +0000

    Fixed the mistake code function

--- Natsunagi/modules/antichannel.py
+++ Natsunagi/modules/antichannel.py
@@ -7,11 +7,10 @@ from telegram.ext.filters import Filters
 
 import Natsunagi.modules.sql.antilinkedchannel_sql as sql
 from Natsunagi import TOKEN, dispatcher
-from Natsunagi.modules.disable import DisableAbleCommandHandler
 from Natsunagi.modules.helper_funcs.anonymous import AdminPerms, user_admin
 from Natsunagi.modules.helper_funcs.chat_status import bot_admin, bot_can_delete
 from Natsunagi.modules.helper_funcs.chat_status import user_admin as u_admin
-from Natsunagi.modules.helper_funcs.decorators import natsunagimsg, natsunagicmd
+from Natsunagi.modules.helper_funcs.decorators import natsunagicmd, natsunagimsg
 from Natsunagi.modules.sql import acm_sql
 
 
@@ -28,10 +27,16 @@ def set_antilinkedchannel(update: Update, context: CallbackContext):
             if sql.status_pin(chat.id):
                 sql.disable_pin(chat.id)
                 sql.enable_pin(chat.id)
-                message.reply_html("Enabled Linked channel deletion and Disabled anti channel pin in {}".format(html.escape(chat.title)))
+                message.reply_html(
+                    "Enabled Linked channel deletion and Disabled anti channel pin in {}".format(
+                        html.escape(chat.title)
+                    )
+                )
             else:
                 sql.enable_linked(chat.id)
-                message.reply_html("Enabled anti linked channel in {}".format(html.escape(chat.title)))
+                message.reply_html(
+                    "Enabled anti linked channel in {}".format(html.escape(chat.title))
+                )
         elif s in ["off", "no"]:
             sql.disable_linked(chat.id)
             message.reply_html(
@@ -157,21 +162,34 @@ def set_antipinchannel(update: Update, context: CallbackContext):
             if sql.status_linked(chat.id):
                 sql.disable_linked(chat.id)
                 sql.enable_pin(chat.id)
-                message.reply_html("Disabled Linked channel deletion and Enabled anti channel pin in {}".format(html.escape(chat.title)))
+                message.reply_html(
+                    "Disabled Linked channel deletion and Enabled anti channel pin in {}".format(
+                        html.escape(chat.title)
+                    )
+                )
             else:
                 sql.enable_pin(chat.id)
-                message.reply_html("Enabled anti channel pin in {}".format(html.escape(chat.title)))
+                message.reply_html(
+                    "Enabled anti channel pin in {}".format(html.escape(chat.title))
+                )
         elif s in ["off", "no"]:
             sql.disable_pin(chat.id)
-            message.reply_html("Disabled anti channel pin in {}".format(html.escape(chat.title)))
+            message.reply_html(
+                "Disabled anti channel pin in {}".format(html.escape(chat.title))
+            )
         else:
             message.reply_text("Unrecognized arguments {}".format(s))
         return
     message.reply_html(
-        "Linked channel message unpin is currently {} in {}".format(sql.status_pin(chat.id), html.escape(chat.title)))
+        "Linked channel message unpin is currently {} in {}".format(
+            sql.status_pin(chat.id), html.escape(chat.title)
+        )
+    )
 
 
-@natsunagimsg(Filters.is_automatic_forward | Filters.status_update.pinned_message, group=113)
+@natsunagimsg(
+    Filters.is_automatic_forward | Filters.status_update.pinned_message, group=113
+)
 def eliminate_linked_channel_msg(update: Update, _: CallbackContext):
     message = update.effective_message
     chat = update.effective_chat
@@ -181,6 +199,8 @@ def eliminate_linked_channel_msg(update: Update, _: CallbackContext):
         message.unpin()
     except TelegramError:
         return
+
+
 __mod_name__ = "AntiChannel"
 
 __help__ = """
