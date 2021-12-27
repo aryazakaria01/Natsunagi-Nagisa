6786fafbf7c886d8512e37354341ab0a2e07129d
aryazakaria01 <aryazakaria01@users.noreply.github.com>
2021-12-27 09:43:53 +0000

    Automated code formatting
    Signed-off-by: aryazakarai01 <88209926+aryazakarai01@users.noreply.github.com>

--- Natsunagi/modules/sql/approve_sql.py
+++ Natsunagi/modules/sql/approve_sql.py
@@ -5,6 +5,7 @@ from sqlalchemy.sql.sqltypes import BigInteger
 
 from Natsunagi.modules.sql import BASE, SESSION
 
+
 class Approvals(BASE):
     __tablename__ = "approval"
     chat_id = Column(String(14), primary_key=True)
