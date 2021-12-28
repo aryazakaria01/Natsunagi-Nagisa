cbb2a8188e153fc6717c26f91e6c3701403c8a23
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-12-28 11:27:40 +0700

    Natsunagi: ud: redesign

--- Natsunagi/modules/ud.py
+++ Natsunagi/modules/ud.py
@@ -4,19 +4,39 @@ from telegram.ext import CallbackContext
 
 from Natsunagi import dispatcher
 from Natsunagi.modules.disable import DisableAbleCommandHandler
+from Natsunagi.modules.helper_funcs.alternate import typing_action
 
-
-def ud(update: Update, context: CallbackContext):
-    message = update.effective_message
-    text = message.text[len("/ud ") :]
-    results = requests.get(
-        f"https://api.urbandictionary.com/v0/define?term={text}",
-    ).json()
+@typing_action
+def ud(update, context):
+    msg = update.effective_message
+    args = context.args
+    text = " ".join(args).lower()
+    if not text:
+        msg.reply_text("Please enter keywords to search!")
+        return
+    elif text == "starry":
+        msg.reply_text("Who is Arya!")
+        return
     try:
-        reply_text = f'*{text}*\n\n{results["list"][0]["definition"]}\n\n_{results["list"][0]["example"]}_'
-    except:
-        reply_text = "No results found."
-    message.reply_text(reply_text, parse_mode=ParseMode.MARKDOWN)
+        results = get(
+            f"http://api.urbandictionary.com/v0/define?term={text}"
+        ).json()
+        reply_text = (
+            f'Word: {text}\nDefinition: {results["list"][0]["definition"]}'
+        )
+        reply_text += f'\n\nExample: {results["list"][0]["example"]}'
+    except IndexError:
+        reply_text = f"Word: {text}\nResults: Sorry could not find any matching results!"
+    ignore_chars = "[]"
+    reply = reply_text
+    for chars in ignore_chars:
+        reply = reply.replace(chars, "")
+    if len(reply) >= 4096:
+        reply = reply[:4096]  # max msg lenth of tg.
+    try:
+        msg.reply_text(reply)
+    except BadRequest as err:
+        msg.reply_text(f"Error! {err.message}")
 
 
 UD_HANDLER = DisableAbleCommandHandler(["ud"], ud, run_async=True)
