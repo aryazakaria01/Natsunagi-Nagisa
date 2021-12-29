5cfd318ee973648cf42622812866cb40f2817dc5
aryazakaria01 <80420642+aryazakaria01@users.noreply.github.com>
2021-12-29 03:02:03 +0000

    Fixed the mistake code function

--- Natsunagi/modules/no_sql/log_channel_db.py
+++ Natsunagi/modules/no_sql/log_channel_db.py
@@ -1,6 +1,5 @@
 from Natsunagi.modules.no_sql import get_collection
 
-
 LOG_DATA = get_collection("LOG_CHANNELS")
 
 CHANNELS = {}
@@ -8,9 +7,7 @@ CHANNELS = {}
 
 def set_chat_log_channel(chat_id, log_channel):
     LOG_DATA.update_one(
-        {'chat_id': chat_id},
-        {"$set": {'log_channel': log_channel}},
-        upsert=True
+        {"chat_id": chat_id}, {"$set": {"log_channel": log_channel}}, upsert=True
     )
     CHANNELS[str(chat_id)] = log_channel
 
@@ -20,7 +17,7 @@ def get_chat_log_channel(chat_id) -> int:
 
 
 def stop_chat_logging(chat_id) -> int:
-    res = LOG_DATA.find_one_and_delete({'chat_id': chat_id})
+    res = LOG_DATA.find_one_and_delete({"chat_id": chat_id})
     if str(chat_id) in CHANNELS:
         del CHANNELS[str(chat_id)]
     return res["log_channel"]
@@ -31,10 +28,7 @@ def num_logchannels() -> int:
 
 
 def migrate_chat(old_chat_id, new_chat_id):
-    LOG_DATA.update_one(
-        {'chat_id': old_chat_id},
-        {"$set": {'chat_id': new_chat_id}}
-    )
+    LOG_DATA.update_one({"chat_id": old_chat_id}, {"$set": {"chat_id": new_chat_id}})
     if str(old_chat_id) in CHANNELS:
         CHANNELS[str(new_chat_id)] = CHANNELS.get(str(old_chat_id))
 
@@ -42,9 +36,8 @@ def migrate_chat(old_chat_id, new_chat_id):
 def __load_log_channels():
     global CHANNELS
     CHANNELS = {
-        str(chat['chat_id']):str(chat['log_channel'])
-        for chat in LOG_DATA.find()
-        }
+        str(chat["chat_id"]): str(chat["log_channel"]) for chat in LOG_DATA.find()
+    }
 
 
 __load_log_channels()
