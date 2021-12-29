df66fff7f81141dc92df244791c9cb6d7815bc23
aryazakaria01 <80420642+aryazakaria01@users.noreply.github.com>
2021-12-29 09:46:37 +0000

    Fixed the mistake code function

--- Natsunagi/modules/warns.py
+++ Natsunagi/modules/warns.py
@@ -51,7 +51,7 @@ CURRENT_WARNING_FILTER_STRING = "<b>Current warning filters in this chat:</b>\n"
 
 # Not async
 def warn(
-        user: User, update: Update, reason: str, message: Message, warner: User = None
+    user: User, update: Update, reason: str, message: Message, warner: User = None
 ) -> Optional[str]:  # sourcery no-metrics
     chat = update.effective_chat
     if is_user_admin(update, user.id):
