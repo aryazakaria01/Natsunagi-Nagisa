392a8896124b17e700047dedaa4147f91093ab61
aryazakaria01 <aryazakaria01@users.noreply.github.com>
2021-12-29 09:10:17 +0000

    Automated code formatting
    Signed-off-by: aryazakarai01 <88209926+aryazakarai01@users.noreply.github.com>

--- Natsunagi/modules/admin.py
+++ Natsunagi/modules/admin.py
@@ -37,6 +37,7 @@ from Natsunagi.modules.helper_funcs.extraction import (
 from Natsunagi.modules.log_channel import loggable
 from Natsunagi.modules.helper_funcs.decorators import natsunagicmd
 
+
 async def is_register_admin(chat, user):
     if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
         return isinstance(
@@ -759,27 +760,40 @@ async def staff(client: Client, message: Message):
     creator = []
     co_founder = []
     admin = []
-    admin_check = await client.get_chat_members(message.chat.id, filter="administrators")
+    admin_check = await client.get_chat_members(
+        message.chat.id, filter="administrators"
+    )
     for x in admin_check:
         # Ini buat nyari co-founder
         if x.status == "administrator" and x.can_promote_members and x.title:
             title = escape(x.title)
             co_founder.append(
-                f" <b>├</b> <a href='tg://user?id={x.user.id}'>{x.user.first_name}</a> <i>- {title}</i>")
+                f" <b>├</b> <a href='tg://user?id={x.user.id}'>{x.user.first_name}</a> <i>- {title}</i>"
+            )
         elif x.status == "administrator" and x.can_promote_members and not x.title:
-            co_founder.append(f" <b>├</b> <a href='tg://user?id={x.user.id}'>{x.user.first_name}</a>")
+            co_founder.append(
+                f" <b>├</b> <a href='tg://user?id={x.user.id}'>{x.user.first_name}</a>"
+            )
         # ini buat nyari admin
         elif x.status == "administrator" and not x.can_promote_members and x.title:
             title = escape(x.title)
-            admin.append(f" <b>├</b> <a href='tg://user?id={x.user.id}'>{x.user.first_name}</a> <i>- {title}</i>")
+            admin.append(
+                f" <b>├</b> <a href='tg://user?id={x.user.id}'>{x.user.first_name}</a> <i>- {title}</i>"
+            )
         elif x.status == "administrator" and not x.can_promote_members and not x.title:
-            admin.append(f" <b>├</b> <a href='tg://user?id={x.user.id}'>{x.user.first_name}</a>")
+            admin.append(
+                f" <b>├</b> <a href='tg://user?id={x.user.id}'>{x.user.first_name}</a>"
+            )
         # ini buat nyari creator
         elif x.status == "creator" and x.title:
             title = escape(x.title)
-            creator.append(f" <b>└</b> <a href='tg://user?id={x.user.id}'>{x.user.first_name}</a> <i>- {title}</i>")
+            creator.append(
+                f" <b>└</b> <a href='tg://user?id={x.user.id}'>{x.user.first_name}</a> <i>- {title}</i>"
+            )
         elif x.status == "creator" and not x.title:
-            creator.append(f" <b>└</b> <a href='tg://user?id={x.user.id}'>{x.user.first_name}</a>")
+            creator.append(
+                f" <b>└</b> <a href='tg://user?id={x.user.id}'>{x.user.first_name}</a>"
+            )
 
     if len(co_founder) == 0 and len(admin) == 0:
         result = "🤴 <b><i>Founder</i></b>\n" + "\n".join(creator)
@@ -788,19 +802,17 @@ async def staff(client: Client, message: Message):
         res_admin = admin[-1].replace("├", "└")
         admin.pop(-1)
         admin.append(res_admin)
-        result = (
-            "🤴 <b><i>Founder</i></b>\n" + "\n".join(creator) + "\n\n"
-            "👮‍♂ <b><i>Admin</i></b>\n" + "\n".join(admin)
-        )
+        result = "🤴 <b><i>Founder</i></b>\n" + "\n".join(
+            creator
+        ) + "\n\n" "👮‍♂ <b><i>Admin</i></b>\n" + "\n".join(admin)
 
     elif len(co_founder) > 0 and len(admin) == 0:
         resco_founder = co_founder[-1].replace("├", "└")
         co_founder.pop(-1)
         co_founder.append(resco_founder)
-        result = (
-            "🤴 <b><i>Founder</i></b>\n" + "\n".join(creator) + "\n\n"
-            "👨‍✈️ <b><i>Co-Founder</i></b>\n" + "\n".join(co_founder)
-        )
+        result = "🤴 <b><i>Founder</i></b>\n" + "\n".join(
+            creator
+        ) + "\n\n" "👨‍✈️ <b><i>Co-Founder</i></b>\n" + "\n".join(co_founder)
 
     else:
         resco_founder = co_founder[-1].replace("├", "└")
