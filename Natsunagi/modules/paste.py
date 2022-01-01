3bd793cb78aeca0bc30c3613b9598528f6d94518
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2022-01-01 23:26:26 +0700

    Paste: test

--- Natsunagi/modules/paste.py
+++ Natsunagi/modules/paste.py
@@ -2,10 +2,10 @@ import asyncio
 import codecs
 import os
 import re
-from io import BytesIO
-
 import aiofiles
 import requests
+
+from io import BytesIO
 from pykeyboard import InlineKeyboard
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, Message
@@ -18,7 +18,7 @@ from Natsunagi.modules.helper_funcs.decorators import natsunagicmd
 from Natsunagi.modules.helper_funcs.misc import upload_text
 from Natsunagi.utils.errors import capture_err
 from Natsunagi.utils.keyboard import ikb
-from Natsunagi.utils.pastebin import epaste, hpaste
+from Natsunagi.utils.pastebin import paste, hpaste
 
 __mod_name__ = "Paste"
 
@@ -42,7 +42,7 @@ async def isPreviewUp(preview: str) -> bool:
 
 @app.on_message(filters.command("bpaste") & ~filters.edited)
 @capture_err
-async def paste_func(_, message: Message):
+async def batpaste(_, message: Message):
     if not message.reply_to_message:
         return await eor(message, text="Reply To A Message With /paste")
     r = message.reply_to_message
@@ -90,30 +90,36 @@ async def paste_func(_, message: Message):
 
 @app.on_message(filters.command("paste") & ~filters.edited)
 @capture_err
-async def epaste_func(_, message: Message):
+async def ezuppaste(_, message):
     if not message.reply_to_message:
-        return await message.reply_text("Reply To A Message With /paste")
+        return await message.reply_text(
+            "Reply To A Message With /paste"
+        )
     m = await message.reply_text("Pasting...")
     if message.reply_to_message.text:
         content = str(message.reply_to_message.text)
     elif message.reply_to_message.document:
         document = message.reply_to_message.document
         if document.file_size > 1048576:
-            return await m.edit("You can only paste files smaller than 1MB.")
+            return await m.edit(
+                "You can only paste files smaller than 1MB."
+            )
         if not pattern.search(document.mime_type):
             return await m.edit("Only text files can be pasted.")
         doc = await message.reply_to_message.download()
         async with aiofiles.open(doc, mode="r") as f:
             content = await f.read()
         os.remove(doc)
-    link = await epaste(content)
+    link = await paste(content)
     preview = link + "/preview.png"
     button = InlineKeyboard(row_width=1)
     button.add(InlineKeyboardButton(text="Paste Link", url=link))
 
     if await isPreviewUp(preview):
         try:
-            await message.reply_photo(photo=preview, quote=False, reply_markup=button)
+            await message.reply_photo(
+                photo=preview, quote=False, reply_markup=button
+            )
             return await m.delete()
         except Exception:
             pass
