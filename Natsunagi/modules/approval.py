a79a496c3cc9b720c85edd6d2567daaef8191d6f
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2022-01-01 22:48:38 +0700

    Update approval.py

--- Natsunagi/modules/approval.py
+++ Natsunagi/modules/approval.py
@@ -104,7 +104,7 @@ def approved(update, context):
     approved_users = sql.list_approved(message.chat_id)
     for i in approved_users:
         member = chat.get_member(int(i.user_id))
-        msg += f"- `{i.user_id}`: {member.user['first_name']}\n"
+        msg += f"× `{i.user_id}`: {member.user['first_name']}\n"
     if msg.endswith("approved.\n"):
         message.reply_text(f"No users are approved in {chat_title}.")
         return ""
