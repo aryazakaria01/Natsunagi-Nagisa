65857e17d102788f898563516cf8f08fa3b03f95
aryazakaria01 <aryazakaria01@users.noreply.github.com>
2022-01-01 17:50:02 +0000

    Automated code formatting
    Signed-off-by: aryazakarai01 <88209926+aryazakarai01@users.noreply.github.com>

--- Natsunagi/modules/helper_funcs/misc.py
+++ Natsunagi/modules/helper_funcs/misc.py
@@ -11,12 +11,12 @@ from typing import Dict, List
 from urllib.parse import urlparse, urljoin, urlunparse
 from uuid import uuid4
 from telegram import (
-    MAX_MESSAGE_LENGTH, 
-    Bot, 
-    InlineKeyboardButton, 
-    InlineKeyboardMarkup, 
-    ParseMode, 
-    InlineQueryResultArticle, 
+    MAX_MESSAGE_LENGTH,
+    Bot,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    ParseMode,
+    InlineQueryResultArticle,
     InputTextMessageContent,
 )
 from telegram.error import TelegramError
@@ -173,15 +173,46 @@ def is_module_loaded(name):
 def upload_text(data: str) -> typing.Optional[str]:
     passphrase = Random.get_random_bytes(32)
     salt = Random.get_random_bytes(8)
-    key = Protocol.KDF.PBKDF2(passphrase, salt, 32, 100000, hmac_hash_module=Hash.SHA256)
+    key = Protocol.KDF.PBKDF2(
+        passphrase, salt, 32, 100000, hmac_hash_module=Hash.SHA256
+    )
     compress = zlib.compressobj(wbits=-15)
-    paste_blob = compress.compress(json.dumps({'paste': data}, separators=(',', ':')).encode()) + compress.flush()
+    paste_blob = (
+        compress.compress(json.dumps({"paste": data}, separators=(",", ":")).encode())
+        + compress.flush()
+    )
     cipher = AES.new(key, AES.MODE_GCM)
-    paste_meta = [[base64.b64encode(cipher.nonce).decode(), base64.b64encode(salt).decode(), 100000, 256, 128, 'aes', 'gcm', 'zlib'], 'syntaxhighlighting', 0, 0]
-    cipher.update(json.dumps(paste_meta, separators=(',', ':')).encode())
+    paste_meta = [
+        [
+            base64.b64encode(cipher.nonce).decode(),
+            base64.b64encode(salt).decode(),
+            100000,
+            256,
+            128,
+            "aes",
+            "gcm",
+            "zlib",
+        ],
+        "syntaxhighlighting",
+        0,
+        0,
+    ]
+    cipher.update(json.dumps(paste_meta, separators=(",", ":")).encode())
     ct, tag = cipher.encrypt_and_digest(paste_blob)
-    resp = requests.post('https://bin.nixnet.services', headers={'X-Requested-With': 'JSONHttpRequest'}, data=json.dumps({'v': 2, 'adata': paste_meta, 'ct': base64.b64encode(ct + tag).decode(), 'meta': {'expire': '1week'}}, separators=(',', ':')))
+    resp = requests.post(
+        "https://bin.nixnet.services",
+        headers={"X-Requested-With": "JSONHttpRequest"},
+        data=json.dumps(
+            {
+                "v": 2,
+                "adata": paste_meta,
+                "ct": base64.b64encode(ct + tag).decode(),
+                "meta": {"expire": "1week"},
+            },
+            separators=(",", ":"),
+        ),
+    )
     data = resp.json()
-    url = list(urlparse(urljoin('https://bin.nixnet.services', data['url'])))
+    url = list(urlparse(urljoin("https://bin.nixnet.services", data["url"])))
     url[5] = base58.b58encode(passphrase).decode()
     return urlunparse(url)
