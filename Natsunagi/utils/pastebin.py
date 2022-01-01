50cf8260b0653bdbf4e0372a5cda677af63b7da7
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2022-01-01 22:57:20 +0700

    Added private paste

--- Natsunagi/utils/pastebin.py
+++ Natsunagi/utils/pastebin.py
@@ -1,10 +1,15 @@
 import json
 import socket
+import requests
+import typing
+import zlib
+import base64
+import base58
+from urllib.parse import urlparse, urljoin, urlunparse
+from Crypto import Random, Hash, Protocol
+from Crypto.Cipher import AES
 from asyncio import get_running_loop
 from functools import partial
-
-import requests
-
 from Natsunagi import BOTLOG_CHATID, LOGGER
 from Natsunagi.utils.http import post
 
@@ -114,3 +119,20 @@ async def pastetext(text_to_print, pastetype=None, extension=None):
     if "error" in response:
         response = await d_paste(text_to_print, extension)
     return response
+
+
+def upload_text(data: str) -> typing.Optional[str]:
+    passphrase = Random.get_random_bytes(32)
+    salt = Random.get_random_bytes(8)
+    key = Protocol.KDF.PBKDF2(passphrase, salt, 32, 100000, hmac_hash_module=Hash.SHA256)
+    compress = zlib.compressobj(wbits=-15)
+    paste_blob = compress.compress(json.dumps({'paste': data}, separators=(',', ':')).encode()) + compress.flush()
+    cipher = AES.new(key, AES.MODE_GCM)
+    paste_meta = [[base64.b64encode(cipher.nonce).decode(), base64.b64encode(salt).decode(), 100000, 256, 128, 'aes', 'gcm', 'zlib'], 'syntaxhighlighting', 0, 0]
+    cipher.update(json.dumps(paste_meta, separators=(',', ':')).encode())
+    ct, tag = cipher.encrypt_and_digest(paste_blob)
+    resp = requests.post('https://bin.nixnet.services', headers={'X-Requested-With': 'JSONHttpRequest'}, data=json.dumps({'v': 2, 'adata': paste_meta, 'ct': base64.b64encode(ct + tag).decode(), 'meta': {'expire': '1week'}}, separators=(',', ':')))
+    data = resp.json()
+    url = list(urlparse(urljoin('https://bin.nixnet.services', data['url'])))
+    url[5] = base58.b58encode(passphrase).decode()
+    return urlunparse(url) 
