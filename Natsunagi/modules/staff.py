79cf7015c7d33ff1bc38edb24eab336fdc5e7c08
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2022-01-04 13:27:01 +0700

    Test

--- Natsunagi/modules/staff.py
+++ Natsunagi/modules/staff.py
@@ -11,6 +11,8 @@ def staff(client: Client, message: Message):
     creator = []
     co_founder = []
     admin = []
+    chat_id = message.chat.id
+    chat_title = message.chat.title
     admin_check = pbot.get_chat_members(message.chat.id, filter="administrators")
     for x in admin_check:
         if x.status == "administrator" and x.can_promote_members and x.title:
@@ -42,13 +44,13 @@ def staff(client: Client, message: Message):
             )
 
     if len(co_founder) == 0 and len(admin) == 0:
-        result = "🤴 <b><i>Founder</i></b>\n" + "\n".join(creator)
+        result = "Staff in <b>{chat_title}</b>\n\n🤴 <b><i>Founder</i></b>\n" + "\n".join(creator)
 
     elif len(co_founder) == 0 and len(admin) > 0:
         res_admin = admin[-1].replace("├", "└")
         admin.pop(-1)
         admin.append(res_admin)
-        result = "🤴 <b><i>Founder</i></b>\n" + "\n".join(
+        result = "Staff in <b>{chat_title}</b>\n\n🤴 <b><i>Founder</i></b>\n" + "\n".join(
             creator
         ) + "\n\n" "👮‍♂ <b><i>Admin</i></b>\n" + "\n".join(admin)
 
@@ -56,7 +58,7 @@ def staff(client: Client, message: Message):
         resco_founder = co_founder[-1].replace("├", "└")
         co_founder.pop(-1)
         co_founder.append(resco_founder)
-        result = "🤴 <b><i>Founder</i></b>\n" + "\n".join(
+        result = "Staff in <b>{chat_title}</b>\n\n🤴 <b><i>Founder</i></b>\n" + "\n".join(
             creator
         ) + "\n\n" "👨‍✈️ <b><i>Co-Founder</i></b>\n" + "\n".join(co_founder)
 
@@ -68,8 +70,8 @@ def staff(client: Client, message: Message):
         co_founder.append(resco_founder)
         admin.append(res_admin)
         result = (
-            "🤴 <b><i>Founder</i></b>\n" + "\n".join(creator) + "\n\n"
+            "Staff in <b>{chat_title}</b>\n\n🤴 <b><i>Founder</i></b>\n" + "\n".join(creator) + "\n\n"
             "👨‍✈️ <b><i>Co-Founder</i></b>\n" + "\n".join(co_founder) + "\n\n"
             "👮‍♂ <b><i>Admin</i></b>\n" + "\n".join(admin)
         )
-    message.reply(result)
+    message.send(result)
