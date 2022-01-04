075acd2097ee21a8c3d1c1616702059151b6b154
aryazakaria01 <aryazakaria01@users.noreply.github.com>
2022-01-04 00:23:32 +0000

    Automated code formatting
    Signed-off-by: aryazakarai01 <88209926+aryazakarai01@users.noreply.github.com>

--- Natsunagi/modules/animechan.py
+++ Natsunagi/modules/animechan.py
@@ -6,40 +6,46 @@ from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
-@app.on_callback_query(call_back_in_filter('quotek'))
+@app.on_callback_query(call_back_in_filter("quotek"))
 def callback_quotek(_, query):
     if query.data.split(":")[1] == "change":
         #         query.message.delete()
-        kk = requests.get('https://animechan.vercel.app/api/random').json()
-        anime = kk['anime']
-        quote = kk['quote']
-        character = kk['character']
+        kk = requests.get("https://animechan.vercel.app/api/random").json()
+        anime = kk["anime"]
+        quote = kk["quote"]
+        character = kk["character"]
         caption = f"""
 **Anime:** `{anime}`
 **Character:** `{character}`
 **Quote:** `{quote}`"""
-        query.message.edit(caption,
-                           reply_markup=InlineKeyboardMarkup([
-                               [
-                                   InlineKeyboardButton(
-                                       "Change Quote", callback_data="quotek:change")
-                               ],
-                           ]))
+        query.message.edit(
+            caption,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            "Change Quote", callback_data="quotek:change"
+                        )
+                    ],
+                ]
+            ),
+        )
 
 
-@app.on_message(filters.command('animequote'))
+@app.on_message(filters.command("animequote"))
 def quote(_, message):
-    kk = requests.get('https://animechan.vercel.app/api/random').json()
-    anime = kk['anime']
-    quote = kk['quote']
-    character = kk['character']
+    kk = requests.get("https://animechan.vercel.app/api/random").json()
+    anime = kk["anime"]
+    quote = kk["quote"]
+    character = kk["character"]
     caption = f"""
 **Anime:** `{anime}`
 **Character:** `{character}`
 **Quote:** `{quote}`"""
-    bot.send_message(message.chat.id,
-                     caption,
-                     reply_markup=InlineKeyboardMarkup([[
-                         InlineKeyboardButton("Change Quote",
-                                              callback_data="quotek:change")
-                     ]]))
+    bot.send_message(
+        message.chat.id,
+        caption,
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton("Change Quote", callback_data="quotek:change")]]
+        ),
+    )
