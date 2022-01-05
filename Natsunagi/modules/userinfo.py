bafee2ed2297038e1291c174dca5440a5c2a41e0
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2022-01-05 07:55:58 +0700

    let's see

--- Natsunagi/modules/userinfo.py
+++ Natsunagi/modules/userinfo.py
@@ -2,10 +2,12 @@ import datetime
 import html
 import platform
 import time
+import git
 from platform import python_version
 
 import requests
 from psutil import boot_time, cpu_percent, disk_usage, virtual_memory
+from git import Repo
 from telegram import (
     MAX_MESSAGE_LENGTH,
     InlineKeyboardButton,
@@ -492,9 +494,9 @@ def stats(update, context):
     status += "*× Uptime:* " + str(botuptime) + "\n"
     status += "*× Database size:* " + str(db_size) + "\n"
     kb = [[InlineKeyboardButton("Ping", callback_data="pingCB")]]
-    # repo = git.Repo(search_parent_directories=True)
-    # sha = repo.head.object.hexsha
-    # status += f"*× Commit*: {sha[0:9]}\n"
+    repo = git.Repo(search_parent_directories=True)
+    sha = repo.head.object.hexsha
+    status += f"*× Commit*: {sha[0:9]}\n"
     try:
         update.effective_message.reply_text(
             status
