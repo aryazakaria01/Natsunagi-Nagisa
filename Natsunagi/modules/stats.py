592cd65c7905f1d71a0491ef135b7d7f080fee3f
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2022-01-05 10:27:53 +0700

    Update stats.py

--- Natsunagi/modules/stats.py
+++ Natsunagi/modules/stats.py
@@ -1,7 +1,11 @@
 import os
 import time
-
 import psutil
+
+from telegram import ParseMode, __version__ as peler
+from platform import python_version as memek
+from telethon import __version__ as tlh
+from pyrogram import __version__ as pyr
 
 import Natsunagi.modules.no_sql.users_db as user_db
 from Natsunagi import StartTime
@@ -19,11 +23,26 @@ async def bot_sys_stats():
     stats = f"""
 arya@MacBook:~$ NatsunagiProBot:
 ------------------
+OS: Debian GNU/Linux 10 (buster) x86_64
+Host: Google Compute Engine
+Kernel: 4.19.0-18-cloud-amd64
 Uptime: {formatter.get_readable_time((bot_uptime))}
+Packages: 343 (dpkg)
+Shell: bash 5.0.3
+Release: 11
+System: IOS
+Node name: macOS
+Machine: macOS Mojave 10.14.6
 Bot: {round(process.memory_info()[0] / 1024 ** 2)} MB
-CPU: {cpu}%
-Ram: {mem}%
+CPU: Intel Xeon (96) @ 2.000GHz
+Memory: 190876MiB / 593872MiB
+CPU Usage: {cpu}%
+Ram Usage: {mem}%
 Disk: {disk}%
+Python version: {memek()}
+Library version: v{peler}
+Telethon version: {tlh}
+Pyrogram version: {pyr}
 Users: {user_db.num_users()} users.
 Groups: {user_db.num_chats()} groups.
 """
