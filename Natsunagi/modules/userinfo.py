57c844be42548d9ec2736224755b8a2d8e40fcc4
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2022-01-05 08:43:33 +0700

    Update userinfo.py

--- Natsunagi/modules/userinfo.py
+++ Natsunagi/modules/userinfo.py
@@ -7,6 +7,7 @@ from platform import python_version
 
 import requests
 from psutil import boot_time, cpu_percent, disk_usage, virtual_memory
+from git.exc import GitCommandError, InvalidGitRepositoryError, NoSuchPathError
 from telegram import (
     MAX_MESSAGE_LENGTH,
     InlineKeyboardButton,
