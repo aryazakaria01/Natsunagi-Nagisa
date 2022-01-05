f9d51cff1c8332f6ece27b93d70d9dac9d8ad45a
Furry Chemistry <84753790+aryazakaria01@users.noreply.github.com>
2022-01-05 20:17:14 +0700

    Update stats.py

--- Natsunagi/modules/stats.py
+++ Natsunagi/modules/stats.py
@@ -23,19 +23,7 @@ async def bot_sys_stats():
     stats = f"""
 arya@MacBook:~$ NatsunagiProBot:
 ------------------
-OS: Debian GNU/Linux 10 (buster) x86_64
-Host: Google Compute Engine
-Kernel: 4.19.0-18-cloud-amd64
 Uptime: {formatter.get_readable_time((bot_uptime))}
-Packages: 343 (dpkg)
-Shell: bash 5.0.3
-Release: 11
-System: IOS
-Node name: macOS
-Machine: macOS Mojave 10.14.6
-Bot: {round(process.memory_info()[0] / 1024 ** 2)} MB
-CPU: Intel Xeon (96) @ 2.000GHz
-Memory: 190876MiB / 593872MiB
 CPU Usage: {cpu}%
 Ram Usage: {mem}%
 Disk: {disk}%
