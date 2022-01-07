da78478e825fcf8e869e069d7b510819bcb23093
Furry Chemistry <84753790+aryazakaria01@users.noreply.github.com>
2022-01-07 15:08:08 +0700

    fucking typos

--- Natsunagi/utils/dbfunctions.py
+++ Natsunagi/utils/dbfunctions.py
@@ -2,7 +2,7 @@ import codecs
 import pickle
 from typing import Dict, List, Union
 
-from Natsunagi.module.no_sql import db
+from Natsunagi.modules.no_sql import db
 
 # SOME THINGS ARE FUCKED UP HERE, LIKE TOGGLEABLES HAVE THEIR OWN COLLECTION
 # (SHOULD FIX IT WITH SOMETHING LIKE TOGGLEDB), MOST OF THE CODE IS BAD AF
