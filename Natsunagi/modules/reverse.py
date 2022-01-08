410cf9f6aa8d1f581d00d69086c4faeb1e26b07b
Furry Chemistry <84753790+aryazakaria01@users.noreply.github.com>
2022-01-08 17:20:05 +0700

    miss import

--- Natsunagi/modules/reverse.py
+++ Natsunagi/modules/reverse.py
@@ -12,6 +12,7 @@ from telegram.ext import CallbackContext
 
 from Natsunagi import dispatcher
 from Natsunagi.modules.disable import DisableAbleCommandHandler
+from Natsunagi.modules.helper_funcs.decorators import natsunagicmd
 
 opener = urllib.request.build_opener()
 useragent = "Mozilla/5.0 (Linux; Android 6.0.1; SM-G920V Build/MMB29K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.98 Mobile Safari/537.36"
