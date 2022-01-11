541fb5c19d10b345ac1b6573a8f3eb4ad98b2ed9
Furry Chemistry <84753790+aryazakaria01@users.noreply.github.com>
2022-01-11 18:17:05 +0700

    K

--- Natsunagi/modules/stickers.py
+++ Natsunagi/modules/stickers.py
@@ -13,7 +13,7 @@ from telegram import TelegramError, Update
 from telegram.ext import CallbackContext
 from telegram.utils.helpers import mention_html
 
-from Natsunagi import dispatcher
+from Natsunagi import dispatcher, telethn as bot
 from Natsunagi.events import register
 from Natsunagi.modules.disable import DisableAbleCommandHandler
 from Natsunagi.modules.helper_funcs.decorators import natsunagicmd
