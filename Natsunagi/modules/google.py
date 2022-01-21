736f2703e83a4cafd29216ef13cc9d278b36a551
Furry Chemistry <84753790+aryazakaria01@users.noreply.github.com>
2022-01-21 22:33:04 +0700

    .

--- Natsunagi/modules/google.py
+++ Natsunagi/modules/google.py
@@ -10,47 +10,12 @@ import bs4
 import requests
 from bing_image_downloader import downloader
 from bs4 import BeautifulSoup
-from geopy.geocoders import Nominatim
 from requests import get, post
 from search_engine_parser import GoogleSearch
 from telethon.tl import functions, types
 
 from Natsunagi import BOT_NAME, telethn
 from Natsunagi.events import register
-
-
-@register(pattern="^/gps (.*)")
-async def _(event):
-    if event.fwd_from:
-        return
-    if event.is_group and not await is_register_admin(
-        event.input_chat, event.message.sender_id
-    ):
-        await event.reply("You are not Admin. So, You can't use this. Try in my inbox")
-        return
-
-    args = event.pattern_match.group(1)
-
-    try:
-        geolocator = Nominatim(user_agent="SkittBot")
-        location = args
-        geoloc = geolocator.geocode(location)
-        longitude = geoloc.longitude
-        latitude = geoloc.latitude
-        gm = "https://www.google.com/maps/search/{},{}".format(latitude, longitude)
-        await telethn.send_file(
-            event.chat_id,
-            file=types.InputMediaGeoPoint(
-                types.InputGeoPoint(float(latitude), float(longitude))
-            ),
-        )
-        await event.reply(
-            "Open with: [Google Maps]({})".format(gm),
-            link_preview=False,
-        )
-    except Exception as e:
-        print(e)
-        await event.reply("I can't find that")
 
 
 @register(pattern="^/img (.*)")
